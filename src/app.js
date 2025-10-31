477845e6d39a16a6f7e22e1f091c46aa77ca6d09
Adi9876 <begin1625@gmail.com>
2025-11-01 01:10:47 +0530

    Secure API with arcjet

--- src/app.js
+++ src/app.js
@@ -1,9 +1,48 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.route.js';
+import securityMiddleware from './middleware/security.middleware.js';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(securityMiddleware);
+
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => {
+        logger.info(message.trim());
+      },
+    },
+  })
+);
+
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
+  logger.info('Hello from acquistions');
   res.status(200).send('Hello from acquistions');
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisition API is running !' });
 });
 
 export default app;
